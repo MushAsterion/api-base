37f40dc863fa7eb17122abf23441b9dea11d7e3f
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2023-09-27 15:40:56 +0200

    Fixed order to have token stored even if verify failed

--- hooks/authenticate.js
+++ hooks/authenticate.js
@@ -9,9 +9,9 @@ export default async (request, reply) => {
 
     try {
         const token = request.headers.authorization.split(' ')[1];
-        const decoded = await request.jwtVerify(token);
-
         request.userData.auth.jwt = token;
+
+        const decoded = await request.jwtVerify();
         request.userData.auth.user = await User.findById(decoded.id).populate({ path: 'roles' }).exec();
 
         await parsePermissions(request, '');
